695f7cad6bea732704cd1b861d3d2bbd0fe26e13
Liam Timms <timms5000@gmail.com>
2022-08-09 18:18:17 -0400

    CLI parser, numpy data saving, parallel checking
    the parallel implementation is slower than just the loop
    CLI parser only takes a few of the parameters
    and numpy data saving is does not allow choice of save location

--- kuramoto-rust/src/main.rs
+++ kuramoto-rust/src/main.rs
@@ -3,11 +3,35 @@ extern crate ndarray;
 use ndarray::parallel::prelude::*;
 use ndarray::prelude::*;
 use ndarray::Array;
-use ndarray::Zip;
+use ndarray_npy::write_npy;
 use ndarray_rand::rand_distr;
 use ndarray_rand::RandomExt;
-use rayon::prelude::*;
+use std::fmt::Write;
 
+use indicatif::ProgressBar;
+
+use clap::Parser;
+// use ndarray::Zip;
+// use rayon::prelude::*;
+
+/// CLI arguments
+#[derive(Parser, Debug)]
+#[clap(author, version, about, long_about = None)]
+struct Args {
+    /// Number of dimensions of the system
+    #[clap(short, long, value_parser, default_value_t = 1)]
+    dimension: i32,
+
+    /// Number of oscillators
+    #[clap(short, long, value_parser, default_value_t = 20)]
+    number: usize,
+
+    /// Number of steps to run the simulation
+    #[clap(short, long, value_parser, default_value_t = 10000)]
+    tmax: usize,
+}
+
+// Currently unused struct
 struct KuramotoCircle {
     // a 1D circle of oscillators (2nd dimension is the time)
     phi: Array<f64, Ix2>,       // the phase of each oscillator in rads
@@ -18,6 +42,7 @@ struct KuramotoCircle {
 }
 
 fn max(a: &f64, b: &f64) -> f64 {
+    // quick function to return the max of two f64 values
     if *a >= *b {
         *a
     } else {
@@ -26,9 +51,11 @@ fn max(a: &f64, b: &f64) -> f64 {
 }
 
 fn find_delay(i: &usize, j: &usize, n: &usize, z: &f64) -> usize {
+    // find the delay between oscillator i and it's neighbor j
     let x = (*i as f64 - *j as f64).abs();
     let y = *n as f64 - x; // wrap around otherside of the circle
     let d = {
+        // find minimum distance
         if x <= y {
             x
         } else if x > y {
@@ -40,9 +67,9 @@ fn find_delay(i: &usize, j: &usize, n: &usize, z: &f64) -> usize {
     ((z / *n as f64) * d).round() as usize
 }
 
-fn calculate_delays(timemetric: &f64, n: &usize, dt: &f64, dimension: &i32) -> Array<usize, Ix2> {
+fn calculate_delays(timemetric: &f64, n: &usize, dt: &f64) -> Array<usize, Ix2> {
+    // calculate the delays for each oscillator
     let z: f64 = timemetric / dt;
-
     let mut tau: Array<usize, Ix2> = Array::zeros((*n, *n));
     for i in 0..*n {
         for j in 0..*n {
@@ -212,6 +239,33 @@ fn update_oscillator(
         let factor = phi[[j, timewithdelay]] - phi_current;
         summation += kcoupling[[*i, j]] * factor.sin() * connectionmatrix[[*i, j]];
     }
+    let phi_new = phi_current + (omega[*i] + (summation / n as f64)) * dt;
+    phi_new
+}
+
+fn update_oscillator_parallel(
+    t: &usize,
+    i: &usize,
+    phi: &Array<f64, Ix2>,
+    tau: &Array<usize, Ix2>,
+    omega: &Array<f64, Ix1>,
+    kcoupling: &Array<f64, Ix2>,
+    connectionmatrix: &Array<f64, Ix2>,
+    dt: &f64,
+) -> f64 {
+    // SLOWER than update_oscillator in all tests
+    let phi_current = phi[[*i, *t]];
+    let n = phi.shape()[0];
+    // based on https://www.anycodings.com/1questions/5406370/how-to-use-rayon-for-parallel-calculation-of-pi
+    let summation: f64 = (0..n)
+        .into_par_iter()
+        .map(|j| {
+            let timewithdelay = t - tau[[*i, j]] as usize;
+            let factor = phi[[j, timewithdelay]] - phi_current;
+            ((kcoupling[[*i, j]] * factor.sin() * connectionmatrix[[*i, j]]) as f64) as f64
+        })
+        .reduce(|| 0.0, |a, b| a + b);
+
     let phi_new = phi_current + (omega[*i] + (summation / n as f64)) * dt; /* error here */
     phi_new
 }
@@ -221,35 +275,28 @@ fn model(
     mut phi: Array<f64, Ix2>,
     mut kcoupling: Array<f64, Ix2>,
     omega: &Array<f64, Ix1>,
-
+    // parameters of the simulation itself
     dt: &f64,
     tinitial: &usize,
     tmax: &usize,
     n: &usize,
     epsilon: &f64,
-
     // parameters for connections
     tau: &Array<usize, Ix2>,
     connectionmatrix: &Array<f64, Ix2>,
     alpha: &Array<f64, Ix2>,
 ) -> (Array<f64, Ix2>, Array<f64, Ix2>) {
+    // replaces "function model(dt, tinitial, tmax, N, epsilon, dimension)" in igor
     let mut summation = 0.0;
     let mut phicurrent: Array<f64, Ix1> = Array::zeros(*n);
 
-    //BEGINING OF TIME (1D)
+    let pb = ProgressBar::new(((*tmax - 1) - *tinitial) as u64);
+
+    // BEGINING OF TIME (1D)
     for t in *tinitial..(*tmax - 1) {
-        //Evolution of Phase
-        // for i in 0..*n {
-        //     phicurrent[i] = phi[[i, t]];
-        //     for j in 0..*n {
-        //         let timewithdelay = t - tau[[i, j]] as usize;
-        //         let factor = phi[[j, timewithdelay]] - phicurrent[i];
-        //         summation = summation + kcoupling[[i, j]] * factor.sin() * connectionmatrix[[i, j]];
-        //     }
-        //     let timewithstep = t + 1;
-        //     phi[[i, timewithstep]] = phicurrent[i] + (omega[i] + (summation / *n as f64)) * dt; /* error here */
-        //     summation = 0.0;
-        // }
+        pb.inc(1); // update progressbar
+
+        // Evolution of the Oscillators
         for i in 0..*n {
             phi[[i, t + 1]] = update_oscillator(
                 &t,
@@ -264,17 +311,15 @@ fn model(
         }
 
         // attempt at iteration over all oscillators
-        let phicurrent = phi.slice(s![.., t]);
-
-        for (i, value) in phicurrent.indexed_iter() {
-            println!("value {} for index {} at time {}", value, i, t);
-        }
+        // let phicurrent = phi.slice(s![.., t]);
+        // for (i, value) in phicurrent.indexed_iter() {
+        //     println!("value {} for index {} at time {}", value, i, t);
+        // }
 
         // attempt at parallel iteration over all oscillators
-
-        Zip::indexed(phicurrent).par_for_each(|i, value| {
-            println!("value {} for index {} at time {}", value, i, t);
-        });
+        // Zip::indexed(phicurrent).par_for_each(|i, value| {
+        //     println!("value {} for index {} at time {}", value, i, t);
+        // });
 
         //Evolution of Coupling
         for i in 0..*n {
@@ -285,7 +330,9 @@ fn model(
                     + (epsilon * (alpha[[i, j]] * factor.cos() - kcoupling[[i, j]]) * dt);
             }
         }
-    } //END OF TIME (1D)
+    }
+    // END OF TIME (1D)
+    pb.finish_with_message("Done with modeling");
     return (phi, kcoupling);
 }
 
@@ -306,20 +353,23 @@ fn op_compare(
 
 fn main() {
     // all parameters
-    let dimension = 1;
-    let g = 1.0; //The initial coupling constant
-    let n: usize = 20; //Either the number of oscillators (1D) or the height/width of the square array
-    let epsilon = 0.1;
-    let dt = 0.01;
+    let cli = Args::parse();
+    let dimension = cli.dimension;
+    let n: usize = cli.number; //Either the number of oscillators (1D) or the height/width of the square array
+    let tmax: usize = cli.tmax; //the number of total time steps we are simulating (counts the initial conditions)
+    let g: f64 = 1.0; //The initial coupling constant
+    let epsilon: f64 = 0.1; //Speed of coupling change
+    let dt = 0.01; //the time step we are using
     let timemetric: f64 = 2.0; //Zannette's "T" used only for initial conditions if timemetric2 != it.
-    let tinitial = 100;
+    let tinitial = tmax / 10; // the time step we start the simulation at (tmax/10 to set initial conditions)
+                              // tinitial must be less than tmax and longer than longest time delay in the system
 
-    let mode = 0; //Sets initial conditions: 0->random phases, 1->same phase cluster, 2->plane wave cluster, 3->circles cluster
+    let _mode: i32 = 0; //Sets initial conditions:
+                        // 0->random phases, only one currently implemented
+                        // 1->same phase cluster, 2->plane wave cluster, 3->circles cluster
     let clustersize = 0; //cluster=N for whole array to be driven
     let drivingfrequency: f64 = 1.0; //frequency that the oscillaotrs in the cluster will be forced to move at
     let spreadinomega: f64 = 0.25; //the standard deviation in the natural frequencies
-                                   // let tmax = 2000; //the number of total time steps we are simulating (counts the initial conditions)
-    let tmax = 2000; //the number of total time steps we are simulating (counts the initial conditions)
 
     if dimension == 1 {
         let mut kcoupling: Array<f64, Ix2> = Array::zeros((n, n));
@@ -334,7 +384,7 @@ fn main() {
         let alpha: Array<f64, _> = Array::ones((n, n));
         let connectionmatrix: Array<f64, _> = Array::ones((n, n));
 
-        let tau: Array<usize, Ix2> = calculate_delays(&timemetric, &n, &dt, &dimension);
+        let tau: Array<usize, Ix2> = calculate_delays(&timemetric, &n, &dt);
 
         // TODO: check implmentation of this normal distribution
         let omega: Array<f64, Ix1> =
@@ -342,7 +392,7 @@ fn main() {
 
         // initial conditions
         let mut phi: Array<f64, Ix2> = initialize_phi(&n, &clustersize, &tmax);
-        println!("phi shape: {:?}", phi.shape()[0]);
+        // println!("phi shape: {:?}", phi.shape()[0]);
 
         // driving the cluster for the initial time
         phi = driver(
@@ -369,6 +419,19 @@ fn main() {
             &connectionmatrix,
             &alpha,
         );
+
+        let mut phi_save_name = "phi_".to_string();
+        write!(phi_save_name, "N{}-tmax{}-dim{}.npy", n, tmax, dimension).unwrap();
+        write_npy(&phi_save_name, &phi);
+
+        let mut kcoupling_save_name = "kcoupling_".to_string();
+        write!(
+            kcoupling_save_name,
+            "N{}-tmax{}-dim{}.npy",
+            n, tmax, dimension
+        )
+        .unwrap();
+        write_npy(&kcoupling_save_name, &kcoupling);
 
         // calculating the order parameter
         let (order_parameter, order_parameter2_cluster, order_parameter2_cluster_connected) =
