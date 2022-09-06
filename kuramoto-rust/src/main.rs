161ff80ccd8b854103261e6b7278f33f83637fcd
Liam Timms <timms5000@gmail.com>
2022-09-06 00:15:18 -0400

    more complete CLI with checks

--- kuramoto-rust/src/main.rs
+++ kuramoto-rust/src/main.rs
@@ -27,8 +27,36 @@ struct Args {
     number: usize,
 
     /// Number of steps to run the simulation
-    #[clap(short, long, value_parser, default_value_t = 500)]
+    #[clap(short, long, value_parser, default_value_t = 2000)]
     timesim: usize,
+
+    /// Size of time step using runge-kutta
+    #[clap(long, value_parser, default_value_t = 0.01)]
+    dt: f64,
+
+    /// Time metric. Zannette's "T"
+    #[clap(long, value_parser, default_value_t = 2.0)]
+    timemetric: f64,
+
+    /// Standard deviation in natural frequency distribution
+    #[clap(short, long, value_parser, default_value_t = 0.25)]
+    spreadinomega: f64,
+
+    /// Initial Coupling strength
+    #[clap(short, long, value_parser, default_value_t = 1.0)]
+    g: f64,
+
+    /// Speed of coupling evolution
+    #[clap(short, long, value_parser, default_value_t = 0.1)]
+    epsilon: f64,
+
+    /// driving frequency
+    #[clap(short, long, value_parser, default_value_t = 1.0)]
+    drivingfrequency: f64,
+
+    /// Size of cluster
+    #[clap(short, long, value_parser, default_value_t = 0)]
+    clustersize: usize,
 }
 
 fn main() {
@@ -36,21 +64,29 @@ fn main() {
     let cli = Args::parse();
 
     let dimension = cli.dimension;
+    if dimension != 1 && dimension != 2 {
+        panic!("Dimension must be 1 or 2");
+    }
+
+    let output_dir: PathBuf = PathBuf::from(cli.output_dir);
+    if !output_dir.exists() {
+        panic!("Output directory does not exist");
+    }
+
     let n: usize = cli.number; //Either the number of oscillators (1D) or the height/width of the square array
     let timesim: usize = cli.timesim; //the number of total time steps we are simulating (counts the initial conditions)
-    let output_dir: PathBuf = PathBuf::from(cli.output_dir);
 
     // let dimension = 2;
     // let n: usize = 20; //Either the number of oscillators (1D) or the height/width of the square array
     // let tmax: usize = 2000; //the number of total time steps we are simulating (counts the initial conditions)
 
-    let dt = 0.01; //the time step we are using
-                   //
-    let spreadinomega: f64 = 0.25; //the standard deviation in the natural frequencies
-                                   //
-    let g: f64 = 1.0; //The initial coupling constant
-    let epsilon: f64 = 0.1; //Speed of coupling change
-    let timemetric: f64 = 2.0; //Zannette's "T" used only for initial conditions if timemetric2 != it.
+    let dt = cli.dt; //the time step we are using
+
+    let spreadinomega: f64 = cli.spreadinomega; //the standard deviation in the natural frequencies
+                                                //
+    let g: f64 = cli.g; //The initial coupling constant
+    let epsilon: f64 = cli.epsilon; //Speed of coupling change
+    let timemetric: f64 = cli.timemetric; //Zannette's "T" used only for initial conditions if timemetric2 != it.
 
     // let tinitial = 200; // the time step we start the simulation at
     // tinitial must be less than tmax and longer than longest time delay in the system so we
@@ -61,8 +97,11 @@ fn main() {
                         // 1 -> same phase cluster,
                         // 2 -> plane wave cluster,
                         // 3 -> circles cluster
-    let clustersize = 0; //cluster=N for whole array to be driven
-    let drivingfrequency: f64 = 1.0; //frequency that the oscillaotrs in the cluster will be forced to move at
+    let clustersize: usize = cli.clustersize; //cluster=N for whole array to be driven
+    if clustersize > n {
+        panic!("Cluster size must be less than or equal to the number of oscillators");
+    }
+    let drivingfrequency: f64 = cli.drivingfrequency; //frequency that the oscillaotrs in the cluster will be forced to move at
 
     // setting up the output file name for later
     let mut phi_save_name = "phi_".to_string();
